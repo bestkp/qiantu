cbc467cf5279b2e590e30672df98f5e210fd6a9f
bestkp <919375699@qq.com>
2015-07-27 22:00:29 +0800

    test post

---
+++ JQuery/aaa.php
@@ -0,0 +1,7 @@
+<?php
+    if($_GET['url']=='ycku'){
+        echo '票程俱乐部';
+    }else{
+        echo '俱乐部俱乐部';
+    }
+?>
---
+++ JQuery/jsonp.php
@@ -0,0 +1,5 @@
+<?php
+    $_arr=array('a'=>1,'b'=>'sdf','c'=>3);
+    $_result=json_encode($_arr);
+    echo $_result;
+?>
